2e7ffcfda624f2bc9680a00bda2b3367efd7ab6d
we1per <na0k1kuwa71@gmail.com>
2026-01-25 21:59:22 +0900

    Tapping Term変更
    たまにHold判定されないときがあるため

--- qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
+++ qmk_firmware/keyboards/keyball/keyball39/keymaps/via/config.h
@@ -40,7 +40,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define AUTO_MOUSE_DEFAULT_LAYER 6  // AMLはレイヤー6にする
 
 #define HOLD_ON_OTHER_KEY_PRESS_PER_KEY
-#define TAPPING_TERM 250
+#define TAPPING_TERM 220
 #define QUICK_TAP_TERM 120 
 #define AUTO_MOUSE_LAYER_KEEP_TIME 30000  // AML保持時間
 #define TAPPING_TERM_TD 300  // 擬似Tap Dance用のTapping Term
